ca8a38639a75cf75e0221ae4e2d3efd411371b16
William <wjrichards@sky.com>
2020-09-23 15:34:35 +0100

    autopep8

--- components/food.py
+++ components/food.py
@@ -11,10 +11,13 @@ class Food:
 
     def newloc(self, snake):
         cont = True
-        while cont == True:
+        while cont is True:
+
             self.x = numpy.random.randint(10, 490)
             self.y = numpy.random.randint(10, 490)
+
             min_dist_x = min([self.x - val for val in snake.x])**2 < 15**2
             min_dist_y = min([self.y - val for val in snake.y])**2 < 15**2
+
             if not min_dist_x or not min_dist_y:
                 cont = False
