97e15e6ea640c811ab67a391e695ff555a92c7f1
william <wjrichards@sky.com>
2020-11-01 16:42:09 +0000

    keep playing

--- main.py
+++ main.py
@@ -7,55 +7,57 @@ import time
 
 
 def main():
-    pygame.init()
-    score = 0
+    while True:
+        pygame.init()
+        score = 0
 
-    window = pygame.display.set_mode((500, 500))
-    pygame.display.set_caption("Snake")
+        window = pygame.display.set_mode((500, 500))
+        pygame.display.set_caption("Snake")
 
-    snake = Snake()
-    food = Food()
-    game = True
-    start = False
+        snake = Snake()
+        food = Food()
+        game = True
+        start = False
 
-    while start is False:
-        startScreen(window)
-        pygame.time.delay(10)
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    start = True
-                    window.fill((0, 0, 0))
+        while start is False:
+            startScreen(window)
+            pygame.time.delay(10)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        start = True
+                        window.fill((0, 0, 0))
 
-    while game is True:
+        while game is True:
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game = False
+
+            strokes = pygame.key.get_pressed()
+            snake.setdir(strokes)
+            window.fill((0, 0, 0))
+            snake.move()
+
+            drawsnake(window, snake)
+            # if collides with self or edge end game
+            if snake.checkEdges() or snake.checkSelf():
                 game = False
 
-        strokes = pygame.key.get_pressed()
-        snake.setdir(strokes)
+            if collision(snake, food):
+                food.newloc(snake)
+                snake.grow()
+                score += 1
+
+            drawFood(food, window)
+            pygame.display.update()
+            pygame.time.delay(100)
+
         window.fill((0, 0, 0))
-        snake.move()
 
-        drawsnake(window, snake)
-        # if collides with self or edge end game
-        if snake.checkEdges() or snake.checkSelf():
-            game = False
-
-        if collision(snake, food):
-            food.newloc(snake)
-            snake.grow()
-            score += 1
-
-        drawFood(food, window)
-        pygame.display.update()
-        pygame.time.delay(100)
-
-    window.fill((0, 0, 0))
-
-    overScreen(window, score)
-    time.sleep(5)
+        overScreen(window, score)
+        time.sleep(5)
+        
     pygame.quit()
 
 
